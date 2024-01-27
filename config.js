58dc25aa7c04d977e842d1b08522579f9858101e
Bʟ4ᴄᴋUɴ1x_Tᴇᴀᴍ <153548839+bl4ckun1x-team@users.noreply.github.com>
2024-01-27 10:00:31 +0100

    Add password

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const config = {
-  challenge: false,
+  challenge: true,
   users: {
     // username: 'password', you can add multiple users.
-    interstellar: 'password',
+    interstellar: 'h4ck1ng_',
   },
 }
 export default config
