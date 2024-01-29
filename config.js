ba5e6fdf93285972171f1d10bb008a42a598015a
Bʟ4ᴄᴋUɴ1x_Tᴇᴀᴍ <153548839+bl4ckun1x-team@users.noreply.github.com>
2024-01-29 08:48:47 +0100

    reset default config

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const config = {
   challenge: true,
   users: {
-    username: '5eme', // you can add multiple users.
-    '5eme': '5E',
+    // username: 'interstellar', you can add multiple users.
+    interstellar: 'h4ck1ng_',
   },
 }
 export default config
